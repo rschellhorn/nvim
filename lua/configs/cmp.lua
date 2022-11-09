5cb611ae47f4846cd2985fe2f85ada751b92c33b
Rob Schellhorn <rob.schellhorn@gmail.com>
2022-11-09 15:20:23 +0100

    Change cmp completion behave like intellisense

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -54,7 +54,19 @@ cmp.setup {
     ["<Up>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
     ["<Down>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+      if cmp.visible() then
+        local entry = cmp.get_selected_entry()
+        if not entry then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+        else
+          cmp.confirm()
+        end
+      else
+        fallback()
+      end
+    end, {"i","s","c",}),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
